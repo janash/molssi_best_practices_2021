e9cc5ab580f216ab556c5746049ae79c27a5b8f9
Jessica Nash <janash@vt.edu>
2021-05-28 15:24:57 -0400

    python style lesson

--- molecool/measure.py
+++ molecool/measure.py
@@ -4,18 +4,41 @@ Functions that perform measurements.
 
 import numpy as np
 
+
 def calculate_distance(rA, rB):
+    """
+    Calculate the distance between two points.
+
+    Parameters
+    ----------
+    rA, rB : np.ndarray
+        The coordinates of each point.
+
+    Returns
+    -------
+    dist : float
+        The distance between the two points.
+
+    Examples
+    --------
+    >>> r1 = np.array([0, 0, 0])
+    >>> r2 = np.array([0.1, 0, 0])
+    >>> calculate_distance(r1, r2)
+    0.1
+    """
+
     # This function calculates the distance between two points given as numpy arrays.
-    d=(rA-rB)
-    dist=np.linalg.norm(d)
+    d = rA - rB
+    dist = np.linalg.norm(d)
     return dist
+
 
 def calculate_angle(rA, rB, rC, degrees=False):
     # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
     # by setting degrees=True
     AB = rB - rA
     BC = rB - rC
-    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
+    theta = np.arccos(np.dot(AB, BC) / (np.linalg.norm(AB) * np.linalg.norm(BC)))
 
     if degrees:
         return np.degrees(theta)
