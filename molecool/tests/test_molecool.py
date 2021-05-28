934a03067ac5069542689d004d17c5d63fcd7c14
Jessica Nash <janash@vt.edu>
2021-05-28 13:22:32 -0400

    start writing tests for package

--- molecool/tests/test_molecool.py
+++ molecool/tests/test_molecool.py
@@ -6,7 +6,50 @@ Unit and regression test for the molecool package.
 import molecool
 import pytest
 import sys
+import numpy as np
 
 def test_molecool_imported():
     """Sample test, will always pass so long as import statement worked"""
     assert "molecool" in sys.modules
+
+def test_calculate_distance():
+    """Test that calculate_distance function calculates what we expect."""
+    
+    r1 = np.array([0, 0, 0])
+    r2 = np.array([0, 1, 0])
+
+    expected_distance = 1
+
+    calculated_distance = molecool.calculate_distance(r1, r2)
+
+    assert expected_distance == calculated_distance
+
+def test_calculate_angle():
+   """Test the calculate_angle function"""
+
+   r1 = np.array([1, 0, 0])
+   r2 = np.array([0, 0, 0])
+   r3 = np.array([0, 1, 0])
+
+   expected_value = 90
+
+   calculated_value = molecool.calculate_angle(r1, r2, r3, degrees=True)
+   assert expected_value == calculated_value
+
+def test_build_bond_list():
+
+    coordinates = np.array([
+        [1, 1, 1],
+        [2.4, 1, 1],
+        [-0.4, 1, 1],
+        [1, 1, 2.4],
+        [1, 1, -0.4],
+    ])
+
+    bonds = molecool.build_bond_list(coordinates)
+
+    assert len(bonds) == 4
+
+    for bond_length in bonds.values():
+        assert bond_length == 1.4
+
